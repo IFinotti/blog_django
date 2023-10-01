dad13d53c72cc01d5864327d8e5c550715d7c2b0
Isaac Finotti <imisaac2807@gmail.com>
2023-10-01 19:56:38 -0300

    Creating the 'CATEGORY' model

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -22,3 +22,23 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = new_slugify(self.name, 5)
         return super().save(*args, **kwargs)
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(
+        unique=True,
+        default=None,
+        null=True, 
+        blank=True,
+        max_length=255
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = new_slugify(self.name, 5)
+        return super().save(*args, **kwargs)
