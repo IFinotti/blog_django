89068853b59ff47d133dd216556b25264be2a6da
Isaac Finotti <imisaac2807@gmail.com>
2023-10-01 20:12:34 -0300

    Page model and admin

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -23,6 +23,8 @@ class Tag(models.Model):
             self.slug = new_slugify(self.name, 5)
         return super().save(*args, **kwargs)
 
+    def __str__(self) -> str:
+        return self.name
 
 class Category(models.Model):
     class Meta:
@@ -42,3 +44,26 @@ class Category(models.Model):
         if not self.slug:
             self.slug = new_slugify(self.name, 5)
         return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Page(models.Model):
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True,
+        default="", 
+        null=False, 
+        blank=True, 
+        max_length=255
+    )
+    is_published = models.BooleanField(default=False)
+    content = models.TextField
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = new_slugify(self.title, 5)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.title
