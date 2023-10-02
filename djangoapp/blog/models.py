9eefeb17db7a49d80a4d96570209deb5356f84f9
Isaac Finotti <imisaac2807@gmail.com>
2023-10-02 14:59:15 -0300

    Creating the 'Post' model

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -65,7 +65,7 @@ class Page(models.Model):
         default=False, 
         help_text='This field must be marked to the page be show publicly'
     )
-    content = models.TextField
+    content = models.TextField()
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -74,3 +74,30 @@ class Page(models.Model):
     
     def __str__(self) -> str:
         return self.title
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True,
+        default="", 
+        null=False, 
+        blank=True, 
+        max_length=255
+    )
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(
+        default=False, 
+        help_text='This field must be marked to the post be show publicly'
+    )
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m', blank=True, default='')
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text='Do you wanna show the image cover on the post content too??'
+    )
+
+     
