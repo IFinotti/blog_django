dee1af1a06a38466ea3437f48176c3582074bea3
Isaac Finotti <imisaac2807@gmail.com>
2023-10-03 15:45:19 -0300

    Adjusting the post sending

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from blog.models import Post
 
 # Create your views here.
 
@@ -8,6 +9,7 @@ posts = list(range(1000))
 
 
 def index(request):
+    posts = Post.objects.order_by('-pk')
     paginator = Paginator(posts, 9)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
